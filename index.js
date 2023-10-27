065d2a18e73b500b30cc41a32d76a735a707ca9f
MuhammadHassanQureshi543 <102159120+MuhammadHassanQureshi543@users.noreply.github.com>
2023-10-27 16:09:18 +0500

    Update index.js

--- index.js
+++ index.js
@@ -32,57 +32,60 @@ app.listen(PORT, () => {
 // Assuming the Country schema is in a separate file (./model/countryModel)
 const Country = require('./model/countryModel');
 
-app.post('/upload', upload.single('csvFile'), (req, res) => {
-  const file = req.file;
-
-  if (!file) {
-    return res.status(400).send('No file uploaded');
-  }
-
-  const data = file.buffer.toString(); // Assuming the file is in CSV format
-
-  const results = [];
-
-  const lines = data.split('\r\n'); // Changed from '\n' to '\r\n' for Windows-style line endings
-  const headers = lines[0].split(',');
-
-  for (let i = 1; i < lines.length; i++) {
-    const values = lines[i].split(',');
-    const result = {};
-
-    for (let j = 0; j < headers.length; j++) {
-      result[headers[j].trim()] = values[j] ? values[j].trim() : '';
+app.post('/upload', upload.single('csvFile'), async (req, res) => {
+    const file = req.file;
+  
+    if (!file) {
+      return res.status(400).send('No file uploaded');
     }
-
-    results.push(result);
-  }
-
-  Promise.all(
-    results.map(async (row) => {
-      const { Country_Name, Country_two_letter_Code, Currency_Code, Phone_Code } = row;
-      const existingCountry = await Country.findOne({
-        Country_Name,
-        Country_two_letter_Code,
-        Currency_Code,
-        Phone_Code,
-      });
-
-      if (!existingCountry) {
-        await Country.create({
+  
+    const data = file.buffer.toString(); // Assuming the file is in CSV format
+  
+    const results = [];
+  
+    const lines = data.split('\r\n'); // Changed from '\n' to '\r\n' for Windows-style line endings
+    const headers = lines[0].split(',');
+  
+    for (let i = 1; i < lines.length; i++) {
+      const values = lines[i].split(',');
+      const result = {};
+  
+      for (let j = 0; j < headers.length; j++) {
+        result[headers[j].trim()] = values[j] ? values[j].trim() : '';
+      }
+  
+      results.push(result);
+    }
+  
+    try {
+      for (const row of results) {
+        const { Country_Name, Country_two_letter_Code, Currency_Code, Phone_Code } = row;
+        const existingCountry = await Country.findOne({
           Country_Name,
           Country_two_letter_Code,
           Currency_Code,
           Phone_Code,
         });
+  
+        if (!existingCountry) {
+          // Check if result has any non-empty values before creating a new entry
+          if (Object.values(row).some(val => val !== '')) {
+            await Country.create({
+              Country_Name,
+              Country_two_letter_Code,
+              Currency_Code,
+              Phone_Code,
+            });
+          }
+        }
       }
-    })
-  )
-    .then(() => {
+  
       console.log('File processing complete');
       res.send('File uploaded and data processed');
-    })
-    .catch((error) => {
+    } catch (error) {
       console.error('Error processing data:', error);
       res.status(500).send('Internal Server Error');
-    });
-});
+    }
+  });
+  
+  

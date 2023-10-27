21d5ce7f54d471c0942d6bc0a5ce6f38d397c0a4
MuhammadHassanQureshi543 <102159120+MuhammadHassanQureshi543@users.noreply.github.com>
2023-10-27 16:17:01 +0500

    Update index.js

--- index.js
+++ index.js
@@ -34,58 +34,66 @@ const Country = require('./model/countryModel');
 
 app.post('/upload', upload.single('csvFile'), async (req, res) => {
     const file = req.file;
-  
+
     if (!file) {
-      return res.status(400).send('No file uploaded');
+        return res.status(400).send('No file uploaded');
     }
-  
+
     const data = file.buffer.toString(); // Assuming the file is in CSV format
-  
+
     const results = [];
-  
+
     const lines = data.split('\r\n'); // Changed from '\n' to '\r\n' for Windows-style line endings
     const headers = lines[0].split(',');
-  
+
+    // Define the required fields here
+    const requiredFields = ['Country_Name', 'Country_two_letter_Code', 'Currency_Code', 'Phone_Code'];
+
+    // Check if all required fields are present in the headers
+    if (!requiredFields.every(field => headers.includes(field))) {
+        return res.status(400).send('The file does not contain all required fields');
+    }
+
     for (let i = 1; i < lines.length; i++) {
-      const values = lines[i].split(',');
-      const result = {};
-  
-      for (let j = 0; j < headers.length; j++) {
-        result[headers[j].trim()] = values[j] ? values[j].trim() : '';
-      }
-  
-      results.push(result);
-    }
-  
-    try {
-      for (const row of results) {
-        const { Country_Name, Country_two_letter_Code, Currency_Code, Phone_Code } = row;
-        const existingCountry = await Country.findOne({
-          Country_Name,
-          Country_two_letter_Code,
-          Currency_Code,
-          Phone_Code,
-        });
-  
-        if (!existingCountry) {
-          // Check if result has any non-empty values before creating a new entry
-          if (Object.values(row).some(val => val !== '')) {
-            await Country.create({
-              Country_Name,
-              Country_two_letter_Code,
-              Currency_Code,
-              Phone_Code,
-            });
-          }
+        const values = lines[i].split(',');
+        const result = {};
+
+        for (let j = 0; j < headers.length; j++) {
+            result[headers[j].trim()] = values[j] ? values[j].trim() : '';
         }
-      }
-  
-      console.log('File processing complete');
-      res.send('File uploaded and data processed');
-    } catch (error) {
-      console.error('Error processing data:', error);
-      res.status(500).send('Internal Server Error');
+
+        results.push(result);
     }
-  });
-  
+
+    try {
+        for (const row of results) {
+            const { Country_Name, Country_two_letter_Code, Currency_Code, Phone_Code } = row;
+            const existingCountry = await Country.findOne({
+                Country_Name,
+                Country_two_letter_Code,
+                Currency_Code,
+                Phone_Code,
+            });
+
+            if (!existingCountry) {
+                // Check if result has any non-empty values before creating a new entry
+                if (Object.values(row).some(val => val !== '')) {
+                    await Country.create({
+                        Country_Name,
+                        Country_two_letter_Code,
+                        Currency_Code,
+                        Phone_Code,
+                    });
+                }
+            }
+        }
+
+        console.log('File processing complete');
+        res.send('File uploaded and data processed');
+    } catch (error) {
+        console.error('Error processing data:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
   
